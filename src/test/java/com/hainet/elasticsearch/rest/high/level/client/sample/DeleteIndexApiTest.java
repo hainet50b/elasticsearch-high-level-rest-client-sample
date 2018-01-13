3b5bf5e2d645c6e7296918008a6c38c26960d3d0
hainet <20400616+hainet@users.noreply.github.com>
2018-01-13 20:03:27 +0900

    テストクラスを分割

---
+++ src/test/java/com/hainet/elasticsearch/rest/high/level/client/sample/DeleteIndexApiTest.java
@@ -0,0 +1,64 @@
+package com.hainet.elasticsearch.rest.high.level.client.sample;
+
+import org.apache.http.HttpHost;
+import org.elasticsearch.action.ActionListener;
+import org.elasticsearch.action.admin.indices.delete.DeleteIndexRequest;
+import org.elasticsearch.action.admin.indices.delete.DeleteIndexResponse;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.common.unit.TimeValue;
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class DeleteIndexApiTest {
+
+    @Test
+    public void deleteIndexApiSyncTest() {
+        try (final RestHighLevelClient client = new RestHighLevelClient(
+                RestClient.builder(
+                        new HttpHost("localhost", 9200, "http")
+                )
+        )) {
+            final DeleteIndexRequest request = new DeleteIndexRequest()
+                    .indices("index")
+                    .timeout(TimeValue.timeValueMinutes(2));
+
+            DeleteIndexResponse response = client.indices().deleteIndex(request);
+            assertThat(response.isAcknowledged(), is(true));
+
+        } catch (final IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Test
+    public void deleteIndexApiAsyncTest() {
+        try (final RestHighLevelClient client = new RestHighLevelClient(
+                RestClient.builder(
+                        new HttpHost("localhost", 9200, "http")
+                )
+        )) {
+            final DeleteIndexRequest request = new DeleteIndexRequest()
+                    .indices("index")
+                    .timeout(TimeValue.timeValueMinutes(2));
+
+            client.indices().deleteIndexAsync(request, new ActionListener<DeleteIndexResponse>() {
+                @Override
+                public void onResponse(final DeleteIndexResponse response) {
+                    assertThat(response.isAcknowledged(), is(true));
+                }
+
+                @Override
+                public void onFailure(final Exception e) {
+                    throw new RuntimeException(e);
+                }
+            });
+        } catch (final IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
--- src/test/java/com/hainet/elasticsearch/rest/high/level/client/sample/IndexApiTest.java
+++ src/test/java/com/hainet/elasticsearch/rest/high/level/client/sample/IndexApiTest.java
@@ -3,10 +3,6 @@ package com.hainet.elasticsearch.rest.high.level.client.sample;
 import org.apache.http.HttpHost;
 import org.elasticsearch.action.ActionListener;
 import org.elasticsearch.action.DocWriteResponse;
-import org.elasticsearch.action.admin.indices.delete.DeleteIndexRequest;
-import org.elasticsearch.action.admin.indices.delete.DeleteIndexResponse;
-import org.elasticsearch.action.get.GetRequest;
-import org.elasticsearch.action.get.GetResponse;
 import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.action.index.IndexResponse;
 import org.elasticsearch.client.RestClient;
@@ -24,53 +20,7 @@ import static org.hamcrest.Matchers.greaterThan;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
-public class ElasticsearchRestHighLevelClientSampleTest {
-
-    @Test
-    public void deleteIndexApiSyncTest() {
-        try (final RestHighLevelClient client = new RestHighLevelClient(
-                RestClient.builder(
-                        new HttpHost("localhost", 9200, "http")
-                )
-        )) {
-            final DeleteIndexRequest request = new DeleteIndexRequest()
-                    .indices("index")
-                    .timeout(TimeValue.timeValueMinutes(2));
-
-            DeleteIndexResponse response = client.indices().deleteIndex(request);
-            assertThat(response.isAcknowledged(), is(true));
-
-        } catch (final IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    @Test
-    public void deleteIndexApiAsyncTest() {
-        try (final RestHighLevelClient client = new RestHighLevelClient(
-                RestClient.builder(
-                        new HttpHost("localhost", 9200, "http")
-                )
-        )) {
-            final DeleteIndexRequest request = new DeleteIndexRequest()
-                    .indices("index")
-                    .timeout(TimeValue.timeValueMinutes(2));
-
-            client.indices().deleteIndexAsync(request, new ActionListener<DeleteIndexResponse>() {
-                @Override
-                public void onResponse(final DeleteIndexResponse response) {
-                    assertThat(response.isAcknowledged(), is(true));
-                }
-
-                @Override
-                public void onFailure(final Exception e) {
-                    throw new RuntimeException(e);
-                }
-            });
-        } catch (final IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
+public class IndexApiTest {
 
     @Test
     public void indexApiSyncTest() {
@@ -164,58 +114,4 @@ public class ElasticsearchRestHighLevelClientSampleTest {
             throw new RuntimeException(e);
         }
     }
-
-    @Test
-    public void getApiSyncTest() {
-        try (final RestHighLevelClient client = new RestHighLevelClient(
-                RestClient.builder(
-                        new HttpHost("localhost", 9200, "http")
-                )
-        )) {
-            final GetRequest request = new GetRequest()
-                    .index("index")
-                    .type("logs")
-                    .id("id");
-
-            final GetResponse response = client.get(request);
-
-            assertThat(response.getIndex(), is("index"));
-            assertThat(response.getType(), is("logs"));
-            assertThat(response.getId(), is("id"));
-        } catch (final IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    @Test
-    public void getApiAsyncTest() {
-        try (final RestHighLevelClient client = new RestHighLevelClient(
-                RestClient.builder(
-                        new HttpHost("localhost", 9200, "http")
-                )
-        )) {
-            final GetRequest request = new GetRequest()
-                    .index("index")
-                    .type("logs")
-                    .id("id");
-
-            client.getAsync(request, new ActionListener<GetResponse>() {
-                @Override
-                public void onResponse(final GetResponse response) {
-                    assertThat(response.getIndex(), is("index"));
-                    assertThat(response.getType(), is("logs"));
-                    assertThat(response.getId(), is("id"));
-                }
-
-                @Override
-                public void onFailure(final Exception e) {
-                    throw new RuntimeException(e);
-                }
-            });
-        } catch (final IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-
 }
